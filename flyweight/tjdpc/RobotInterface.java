1119b4087348230790b8c5ea529f95da58bc9d93
Bob Kraemer <bob.kraemer.git@gmail.com>
2024-04-23 12:50:33 -0400

    Template, Null Object, Visitor, Strategy, State, Observer, Memento, Mediator, Iterator, Interpreter, Command, ChainOfResponsibility, Proxy, Flyweight verified

---
+++ flyweight/tjdpc/RobotInterface.java
@@ -0,0 +1,24 @@
+package tjdpc;
+
+public interface RobotInterface {
+    void print();
+
+    void setColor(String colorOfRobot);  // extrinsic data is passed as arguments
+}
+
+class Robot implements RobotInterface  {
+
+    String robotType;
+    public String colorOfRobot;
+
+    public Robot(String robotType) {
+        this.robotType=robotType;
+    }
+    public void setColor(String colorOfRobot) {
+        this.colorOfRobot=colorOfRobot;
+    }
+    @Override
+    public void print() {
+        System.out.println(" This is a " + robotType + " type robot with "+ colorOfRobot + "color");
+    }
+}
