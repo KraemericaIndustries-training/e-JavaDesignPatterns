31a91bf027eb37f96c28cad39b3d69158034562c
Bob Kraemer <bob.kraemer.git@gmail.com>
2024-04-27 11:08:27 -0400

    Template, Null Object, Visitor, Strategy, State, Observer, Memento, Mediator, Iterator, Interpreter, Command, ChainOfResponsibility, Proxy, Flyweight, Facade, Prototype, Decorator verified

--- decorator/jdpSolid/HtmlEncodedMessage.java
+++ decorator/jdpSolid/HtmlEncodedMessage.java
@@ -3,7 +3,6 @@ package jdpSolid;
 import org.apache.commons.lang3.StringEscapeUtils;
 
 //Decorator. Implements component interface
-@SuppressWarnings("deprecation")
 public class HtmlEncodedMessage implements Message {
 
 	private Message msg;
@@ -12,7 +11,6 @@ public class HtmlEncodedMessage implements Message {
 		this.msg = msg;
 	}
 	
-	@SuppressWarnings("deprecation")
 	@Override
 	public String getContent() {
 		return StringEscapeUtils.escapeHtml4(msg.getContent());
